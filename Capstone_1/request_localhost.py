34821f2115b3d2c449dd1a00f84bf8bd85a0ce64
Chewye <werfa13q@gmail.com>
2022-12-19 16:53:43 +0500

    feat: add dockerfile and request script, and fix server flask

---
+++ Capstone_1/request_localhost.py
@@ -0,0 +1,24 @@
+import requests
+
+
+url = 'http://localhost:9696/'
+
+data = {'type': 'Участок',
+ 'weight': 87.0,
+ 'mailctg': 1,
+ 'directctg': 2,
+ 'price_mfi': 150.0,
+ 'dist_qty_oper_login_1': 42.0,
+ 'total_qty_oper_login_1': 720176.0,
+ 'total_qty_oper_login_0': 58950.0,
+ 'total_qty_over_index_and_type': 779126.0,
+ 'total_qty_over_index': 8290896.0,
+ 'is_wrong_rcpn_name': 0,
+ 'is_wrong_phone_number': 0,
+ 'oper_type': 1043}
+
+
+response = requests.post(url, json=data).json()
+
+
+print(response)
--- Capstone_1/server_flask.py
+++ Capstone_1/server_flask.py
@@ -19,7 +19,7 @@ def predict():
     if y_pred >= thr:
         return jsonify('loss')
     else:
-        return jsonify('no loss')
+        return jsonify('not loss')
 
 if __name__=="__main__":
    app.run(debug=True, host='0.0.0.0', port=9696) 
