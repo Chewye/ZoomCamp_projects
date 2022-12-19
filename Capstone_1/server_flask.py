133ab3eb94841a7a9f8a4db57689d40ba75c2532
Chewye <werfa13q@gmail.com>
2022-12-19 16:32:18 +0500

    feat: add server flask and requirements

---
+++ Capstone_1/server_flask.py
@@ -0,0 +1,25 @@
+import pickle
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+with open('model.bin', 'rb') as f_in:  
+    model = pickle.load(f_in)
+
+with open('thr.bin', 'rb') as f_in:  
+    thr = pickle.load(f_in)
+
+app = Flask(__name__) 
+@app.route('/', methods=['POST'])
+
+
+def predict():    
+    data = request.get_json()
+    y_pred = model.predict_proba(list(data.values()))[1]
+    if y_pred >= thr:
+        return jsonify('loss')
+    else:
+        return jsonify('no loss')
+
+if __name__=="__main__":
+   app.run(debug=True, host='0.0.0.0', port=9696) 
